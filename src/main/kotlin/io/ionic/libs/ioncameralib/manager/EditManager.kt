e07a165010acdc0b9b99fd60b438ea0f5badf699
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2026-03-04 12:48:09 -0400

    feat: add saveToGallery to editURIPhoto

--- src/main/kotlin/io/ionic/libs/ioncameralib/manager/EditManager.kt
+++ src/main/kotlin/io/ionic/libs/ioncameralib/manager/EditManager.kt
@@ -1,9 +1,14 @@
 package io.ionic.libs.ioncameralib.manager
 
 import android.app.Activity
+import android.content.ContentResolver
+import android.content.ContentValues
 import android.content.Intent
 import android.graphics.drawable.Drawable
 import android.net.Uri
+import android.os.Build
+import android.os.Environment
+import android.provider.MediaStore
 import android.util.Base64
 import android.util.Log
 import androidx.activity.result.ActivityResultLauncher
@@ -17,14 +22,21 @@ import io.ionic.libs.ioncameralib.model.IONEditParameters
 import io.ionic.libs.ioncameralib.view.ImageEditorActivity
 import java.io.File
 import androidx.core.net.toUri
+import io.ionic.libs.ioncameralib.helper.OSCAMRGalleryHelper
+import java.io.FileNotFoundException
+import java.io.IOException
+import java.io.InputStream
+import java.text.SimpleDateFormat
+import java.util.Date
 
 /**
  * Contains edit functions
  */
 class EditManager(
     private var applicationId: String,
+    private var authority: String,
     private var fileHelper: OSCAMRFileHelperInterface,
-    private var imageHelper: OSCAMRImageHelperInterface
+    private var imageHelper: OSCAMRImageHelperInterface,
 ) {
 
     private var croppedUri: Uri? = null
@@ -37,10 +49,13 @@ class EditManager(
         private const val PNG_TYPE = "png"
         private const val JPEG_EXTENSION = ".$JPEG_TYPE"
         private const val PNG_EXTENSION = ".$PNG_TYPE"
-        private const val AUTHORITY = ".camera.provider"
         private const val IMAGE_MAX_RESOLUTION = 1080
         private const val IMAGE_MAX_QUALITY = 100
         private const val LOG_TAG = "EditManager"
+        private const val TIME_FORMAT = "yyyyMMdd_HHmmss"
+
+        private const val PNG_MIME_TYPE = "image/png"
+        private const val JPEG_MIME_TYPE = "image/jpeg"
     }
 
     /**
@@ -108,7 +123,7 @@ class EditManager(
         }
         val pictureUri = fileHelper.getUriForFile(
             activity,
-            "$applicationId$AUTHORITY",
+            "$applicationId$authority",
             imageFile
         )
         try {
@@ -174,7 +189,7 @@ class EditManager(
             val imageFile = File(resultImagePath)
             val resultImageUri = fileHelper.getUriForFile(
                 activity,
-                "$applicationId$AUTHORITY",
+                "$applicationId$authority",
                 imageFile
             )
             if (resultImageUri == null) {
@@ -194,9 +209,10 @@ class EditManager(
                 return
             }
             if (editParameters.saveToGallery) {
-                // Note: savePictureInGallery functionality would need to be added or delegated
-                // For now, this is a placeholder comment
-                Log.d(LOG_TAG, "Save to gallery requested but not implemented in EditManager")
+                savePictureInGallery(
+                    activity,
+                    if (fileHelper.getFileExtension(resultImagePath) == JPEG_TYPE) 0 else 1,
+                    resultImageUri)
             }
             onMediaResult(mediaResult)
         } else {
@@ -285,6 +301,92 @@ class EditManager(
         }
 
         return IONMediaResult(IONMediaType.PICTURE.type, imagePath, base64Image, metadata, true)
+    }
+
+    private fun savePictureInGallery(activity: Activity, encodingType: Int, srcUri: Uri?) {
+        val galleryPathVO: OSCAMRGalleryHelper = getPicturesPath(encodingType)
+        val fileFromGalleryPath = File(galleryPathVO.galleryPath)
+        val galleryUri = Uri.fromFile(fileFromGalleryPath)
+
+        if (Build.VERSION.SDK_INT <= 28) {
+            writeTakenPictureToGalleryLowerThanAndroidQ(activity, srcUri, galleryUri)
+        } else {
+            writeTakenPictureToGalleryStartingFromAndroidQ(
+                activity,
+                srcUri,
+                galleryPathVO,
+                encodingType
+            )
+        }
+    }
+
+    private fun getPicturesPath(encodingType: Int): OSCAMRGalleryHelper {
+        val timeStamp =
+            SimpleDateFormat(TIME_FORMAT).format(
+                Date()
+            )
+        val imageFileName =
+            "IMG_" + timeStamp + if (encodingType == JPEG) JPEG_EXTENSION else PNG_EXTENSION
+        val storageDir = Environment.getExternalStoragePublicDirectory(
+            Environment.DIRECTORY_PICTURES
+        )
+        storageDir.mkdirs()
+        return OSCAMRGalleryHelper(storageDir.absolutePath, imageFileName)
+    }
+
+    @Throws(IOException::class)
+    private fun writeTakenPictureToGalleryLowerThanAndroidQ(activity: Activity?, srcUri: Uri?, galleryUri: Uri?) {
+        writeUncompressedImage(activity, srcUri, galleryUri)
+        fileHelper.refreshGallery(activity, galleryUri)
+    }
+
+    @Throws(IOException::class)
+    private fun writeTakenPictureToGalleryStartingFromAndroidQ(
+        activity: Activity?,
+        srcUri: Uri?,
+        galleryPathVO: OSCAMRGalleryHelper,
+        encodingType: Int
+    ) {
+        // Starting from Android Q, working with the ACTION_MEDIA_SCANNER_SCAN_FILE intent is deprecated
+        // https://developer.android.com/reference/android/content/Intent#ACTION_MEDIA_SCANNER_SCAN_FILE
+        // we must start working with the MediaStore from Android Q on.
+        val resolver: ContentResolver? = activity?.contentResolver
+        val contentValues = ContentValues()
+        contentValues.put(MediaStore.MediaColumns.DISPLAY_NAME, galleryPathVO.galleryFileName)
+        contentValues.put(MediaStore.MediaColumns.MIME_TYPE, getMimetypeForFormat(encodingType))
+        val galleryOutputUri =
+            resolver?.insert(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, contentValues)
+        val fileStream: InputStream? =
+            fileHelper.getInputStreamFromUriString(
+                srcUri.toString(),
+                activity
+            )
+        fileHelper.writeUncompressedImage(activity, fileStream, galleryOutputUri)
+    }
+
+    /**
+     * In the special case where the default width, height and quality are unchanged
+     * we just write the file out to disk saving the expensive Bitmap.compress function.
+     *
+     * @param src
+     * @throws FileNotFoundException
+     * @throws IOException
+     */
+    @Throws(FileNotFoundException::class, IOException::class)
+    private fun writeUncompressedImage(activity: Activity?, src: Uri?, dest: Uri?) {
+        val fis: InputStream? = fileHelper.getInputStreamFromUriString(src.toString(), activity)
+        fileHelper.writeUncompressedImage(activity, fis, dest)
+    }
+
+    /**
+     * Converts output image format int value to string value of mime type.
+     * @param outputFormat int Output format of camera API.
+     * Must be value of either JPEG or PNG constant
+     * @return String String value of mime type or empty string if mime type is not supported
+     */
+    private fun getMimetypeForFormat(outputFormat: Int): String? {
+        if (outputFormat == PNG) return PNG_MIME_TYPE
+        return if (outputFormat == JPEG) JPEG_MIME_TYPE else ""
     }
 
 }
