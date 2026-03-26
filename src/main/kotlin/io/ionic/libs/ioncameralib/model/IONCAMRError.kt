4fc7e37c9dcd7a2b2f46c4e08a448523d96d3825
Alexandre Jacinto <alexandre.jacinto@outsystems.com>
2026-03-26 17:24:30 -0400

    refactor: remove and update errors

--- src/main/kotlin/io/ionic/libs/ioncameralib/model/IONCAMRError.kt
+++ src/main/kotlin/io/ionic/libs/ioncameralib/model/IONCAMRError.kt
@@ -3,14 +3,14 @@ package io.ionic.libs.ioncameralib.model
 enum class IONCAMRError(val code: Int, val description: String) {
     CAMERA_PERMISSION_DENIED_ERROR(3, "Couldn't access camera. Check your camera permissions and try again."),
     GALLERY_PERMISSION_DENIED_ERROR(6, "Couldn't access your photo gallery because access wasn't provided."),
-    NO_PICTURE_TAKEN_ERROR(7, "No picture captured."),
+    NO_PICTURE_TAKEN_ERROR(7, "Couldn't take photo because the process was canceled."),
     NO_CAMERA_AVAILABLE_ERROR(8, "No camera available."),
     EDIT_IMAGE_ERROR (10, "Couldn't edit image."),
-    TAKE_PHOTO_ERROR(11, "Couldn't capture picture."),
+    TAKE_PHOTO_ERROR(11, "Couldn't take photo."),
     PROCESS_IMAGE_ERROR(13, "Couldn't process image."),
-    EDIT_CANCELLED_ERROR(14, "Couldn't edit picture because the process was canceled."),
-    CAPTURE_VIDEO_ERROR(18, "Couldn't capture video."),
-    CAPTURE_VIDEO_CANCELLED_ERROR(19, "Couldn't capture video because the process was canceled."),
+    EDIT_CANCELLED_ERROR(14, "Couldn't edit photo because the process was canceled."),
+    CAPTURE_VIDEO_ERROR(18, "Couldn't record video."),
+    CAPTURE_VIDEO_CANCELLED_ERROR(19, "Couldn't record video because the process was canceled."),
     GENERIC_CHOOSE_MULTIMEDIA_ERROR(21, "Couldn't choose media from the gallery."),
     CHOOSE_MULTIMEDIA_CANCELLED_ERROR(23, "Couldn't choose media from the gallery because the process was canceled."),
     MEDIA_PATH_ERROR(24, "Couldn't get media file path."),
@@ -20,7 +20,6 @@ enum class IONCAMRError(val code: Int, val description: String) {
     FETCH_IMAGE_FROM_URI_ERROR(31, "Couldn't retrieve image from the URI."),
     FAILED_TO_CAPTURE_IMAGE_ERROR(36, "Failed to capture image."),
     VIDEO_CAPTURE_NOT_SUPPORTED_ERROR(38, "Video capture not supported."),
-    EDIT_OPERATION_CANCELLED_ERROR(40, "User canceled the edit operation."),
     INVALID_ARGUMENT_ERROR(41, "Invalid argument provided to plugin method."),
 
     // Overall Android specific
