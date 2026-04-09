ca8a71ca367c2cd846e59e6268f07826f4910831
OS-pedrogustavobilro <pedro.gustavo.bilro@outsystems.com>
2026-04-09 13:35:18 +0100

    refactor: Remove dead code from old native implementation
    Did not get cleaned up in merge commit because of commit order in repo.

--- src/main/kotlin/com/outsystems/plugins/camera/controller/OSCAMRGalleryHelper.kt
+++
@@ -1,7 +0,0 @@
-package com.outsystems.plugins.camera.controller
-
-class OSCAMRGalleryHelper(val picturesDirectory: String?, val galleryFileName: String?) {
-
-    var galleryPath = this.picturesDirectory + "/" + this.galleryFileName
-
-}
